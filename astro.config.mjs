48963dbc9634d1b98016e3fb821f6225242f7d64
Dmitry Dyachkov <git@e965.ru>
2023-08-07 01:50:51 +0500

    chore: remove @astrojs/image and astro-compress plugins

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,18 +1,19 @@
 import { defineConfig } from 'astro/config';
 
 import mdx from '@astrojs/mdx';
-import image from '@astrojs/image';
 import sitemap from '@astrojs/sitemap';
-
-import compress from 'astro-compress';
 
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://twoweeksgame.ru',
-	integrations: [mdx(), image(), sitemap(), compress()],
+	integrations: [mdx(), sitemap()],
 
 	output: 'static',
 	build: {
 		format: 'file',
+	},
+
+	experimental: {
+		assets: true,
 	},
 });
